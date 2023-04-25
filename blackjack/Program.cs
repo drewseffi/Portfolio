c5de14f9e7d3c440a7e3bd5b11e22d0eed75cc3f
drewseffi <98534980+drewseffi@users.noreply.github.com>
2023-04-25 21:53:02 +0100

    Initial push of my blackjack game
    My blackjack game coded in C# made for being played on the command line

---
+++ blackjack/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        String line;
+
+        String[] cards = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+        List<String> playerHand = new List<string>();
+        List<String> dealerHand = new List<string>();
+
+        Console.WriteLine("Welcome to virtual blackjack. Please type 'draw' to draw another card and type 'stand' to stop drawing cards");
+    
+        dealerHand.Add(cards[Randomize()]);
+        playerHand.Add(cards[Randomize()]);
+        Console.WriteLine(playerHand[0]);
+
+        line = Console.ReadLine();
+        if (line != null)
+        {
+            if (line.Equals("draw"))
+            {
+                
+            }
+            else if (line.Equals("stand"))
+            {
+                
+            }
+        }
+        else 
+        {
+            Console.WriteLine("OK then");
+        }
+
+    }
+
+    public static int Randomize()
+    {
+        Random rand = new Random();
+        int randomNumber = rand.Next(0, 13); // generates a random integer between 0 and 12 (inclusive)
+
+        return randomNumber;
+    }
+}
---
+++ blackjack/README.md
@@ -0,0 +1,25 @@
+# Blackjack Project
+
+## Task Aims
+The aims of the project are as such:
+- Create a virtual deck of cards that include aces and face cards.
+- Create a draw card function that will get you a *card* that you will hold in your *hand*.
+- Keep a running total of what your score is so far.
+- Do this for both the user and the dealer.
+- Allow the user to *stand* with the cards that they have
+- When user gets a score over 21 they are out.
+- If at the end of the game the user has a score higher than the dealer they win and if they have a lower score they lose.
+
+## Time Taken
+1 hour
+
+## Technical Advancements Made
+
+## Date
+Started: 24/04/2023
+
+Ended: 
+
+## Developers
+
+Drew Sutherland - https://github.com/drewseffi
