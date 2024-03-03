26d6040627bb3948b443c41b9f4a1007c97158a9
drewseffi <drewsutherland23@gmail.com>
2024-03-03 22:34:23 +0000

    Create MakingChange exercise project

---
+++ MakingChange/MakingChange/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int[] INCREMENTS = { 500, 100, 25, 10, 5, 2, 1 };
+		int[] change = { 0, 0, 0, 0, 0, 0, 0 };
+		int inputAmount = 0;
+
+		Console.WriteLine("What amount would you like exchanged?");
+
+		string currentLine = Console.ReadLine();
+
+		while (currentLine == null || currentLine == "" || int.TryParse(currentLine, out inputAmount) == false)
+		{
+			Console.WriteLine("Please input an amount");
+
+			currentLine = Console.ReadLine();
+		}
+
+		inputAmount = int.Parse(currentLine);
+
+		Console.WriteLine(inputAmount.ToString() + " accepted");
+
+		int index = 0;
+		bool canUse = true;
+
+		while (inputAmount > 0) 
+		{
+			canUse = true;
+			
+			while (canUse)
+			{
+				if (inputAmount - INCREMENTS[index] < 0) 
+				{ 
+					canUse = false;
+				}
+				else
+				{
+					change[index]++;
+					inputAmount -= INCREMENTS[index];
+				} 
+			}
+
+			index++;
+		}
+
+		int i = 0;
+		foreach (int c in change)
+		{
+			Console.WriteLine(INCREMENTS[i] + ": " + c);
+			i++;
+		}
+	}
+
+}
---
+++ MakingChange/MakingChange/README.md
@@ -0,0 +1,18 @@
+﻿# Making Change Project
+
+## Task aims:
+- Have a currency of £500, £100, £25, £10, £5, £2 and £1
+- Allow user to input amount of money
+- Calculate how many of each increment of change is necessary to create the original sum using the largest increments first
+- Output to user how many of each note/coin is exchanged
+
+## Technical advancements made:
+
+## Duration
+Started: 03/03/2024 21:30
+
+Ended: 03/03/2024 22:13
+
+## Developers
+
+Drew Sutherland - https://github.com/drewseffi
